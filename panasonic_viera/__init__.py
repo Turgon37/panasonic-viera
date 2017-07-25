27f7a6f633396840eb95a8eccd507cffa6d8fdd0
Pierre GINDRAUD <pgindraud@gmail.com>
2017-07-25 19:02:52 +0200

    Fix regression in exception errro message

--- panasonic_viera/__init__.py
+++ panasonic_viera/__init__.py
@@ -42,7 +42,7 @@ from .constants import Keys
 from .utils import getLogger
 from .exceptions import RemoteControlException, UserControlException
 
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 
 __all__ = ['RemoteControl', 'Keys', 'getLogger',
     'RemoteControlException',
--- panasonic_viera/exceptions.py
+++ panasonic_viera/exceptions.py
@@ -6,12 +6,24 @@ class RemoteControlException(Exception):
     """
 
     def getCode(self):
+        """Return the error code associated with this exception
+
+        @return [int] code
+        """
         if len(self.args) > 1:
             code = self.args[1]
             if hasattr(code, 'value'):
                 return code.value
             else:
                 return code
+
+    def __str__(self):
+        """Redefined the old behaviour with simple error
+        """
+        if len(self.args) > 0:
+            return self.args[0]
+        else:
+            return super(RemoteControlException, self).__str__()
 
 class UserControlException(RemoteControlException):
     """Exceptions for users usage errors
